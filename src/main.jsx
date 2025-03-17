eca04bdf76259c988fd3ce1fdd59248ea51de623
ASUS CORE i5 <samuelfaka@gmail.com>
2025-03-17 10:52:35 -1100

    ProfileClinets design

--- src/main.jsx
+++ src/main.jsx
@@ -16,15 +16,19 @@ import ChangePassword from './components/Accoount/ChangePassword.jsx'
 import Interest from './components/Tasks/Interest.jsx'
 import CreateTask from './components/Tasks/CreateTask.jsx'
 import AllTask from './components/Tasks/AllTask.jsx'
-import GetTasks from './components/Tasks/GetTasks.jsx'
+// import GetTasks from "./components/Tasks/GetTasks.jsx"
 import Onboarding from './components/Onboording/Onboarding.jsx'
 import Verification from './components/Onboording/Verification.jsx'
-// import "../node_modules/font-awesome/css/font-awesome.min.css";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
 import Home from './components/Home/Home.jsx'
 import Select from './components/Select/Select.jsx'
 import Request from './components/Request/Request.jsx'
 import ProductClick from './components/Clients/ProductClick.jsx'
 import DetailTask from './components/Details/DetailTask.jsx'
+import Post from './components/PostDetail/Post.jsx'
+import All from './components/Allpost/All.jsx'
+import Offer from './components/Alloffer/Offer.jsx'
+import Pro from './components/ProfileClinets/Pro.jsx'
 
 const router = createBrowserRouter([
   {
@@ -42,15 +46,19 @@ const router = createBrowserRouter([
         {path: '/interest', element: <Interest />},
         {path: '/create-task', element: <CreateTask />},
         {path: '/all-task', element: <AllTask />},
-        {path: '/get-tasks', element: <GetTasks/>},
+        // {path: '/getTask', element: <GetTasks />},
         {path: '/verifly', element: <Onboarding/>},
         {path: '/v-type', element: <Verification/>},
         
         {path: '/home', element: <Home />},
-        {path: 'select', element: <Select />},
+        {path: '/select', element: <Select />},
         {path: '/request', element: <Request />},
         {path: '/productclick',  element: <ProductClick />},
-        {path: '/detailTask', element: <DetailTask />}
+        {path: '/detailTask', element: <DetailTask />},
+        {path: '/post', element: <Post />},
+        {path: '/all', element: <All />},
+        {path: '/offer', element: <Offer />},
+        {path: '/pro', element: <Pro />}
     ] 
   }
 ])
